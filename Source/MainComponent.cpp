96f1f30c08a5d2b50459ff13dc7ea28ac55fe631
kcprs <khatsber@gmail.com>
2017-08-29 23:07:07 +0200

    Added the actual code

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,92 +1,103 @@
 /*
-  ==============================================================================
+==============================================================================
 
-    This file was auto-generated!
+This file was auto-generated!
 
-  ==============================================================================
+==============================================================================
 */
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
 //==============================================================================
 /*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
+This component lives inside our window, and this is where you should put all
+your controls and content.
 */
-class MainContentComponent   : public AudioAppComponent
+class MainContentComponent : public AudioAppComponent
 {
 public:
-    //==============================================================================
-    MainContentComponent()
-    {
-        setSize (800, 600);
+	//==============================================================================
+	MainContentComponent()
+	{
+		setSize(800, 600);
 
-        // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
-    }
+		// specify the number of input and output channels that we want to open
+		setAudioChannels(2, 2);
+	}
 
-    ~MainContentComponent()
-    {
-        shutdownAudio();
-    }
+	~MainContentComponent()
+	{
+		shutdownAudio();
+	}
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
-    {
-        // This function will be called when the audio device is started, or when
-        // its settings (i.e. sample rate, block size, etc) are changed.
+	//==============================================================================
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override
+	{
+		amplitude = 0.25;
+		frequency = 440;
+		phase = phaseAtBufferStart = 0;
+		wavetableSize = 1024;
+		increment = frequency * wavetableSize / sampleRate;
 
-        // You can use this function to initialise any resources you might need,
-        // but be careful - it will be called on the audio thread, not the GUI thread.
+		for (int i = 0; i < wavetableSize; ++i)
+		{
+			wavetable.insert(i, sinf(2 * double_Pi * i / wavetableSize));
+		}
+	}
 
-        // For more details, see the help for AudioProcessor::prepareToPlay()
-    }
+	void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) override
+	{
+		for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+		{
+			float* const buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+			phase = phaseAtBufferStart; //This ensures that the phase is the same for each channel
 
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
-    {
-        // Your audio-processing code goes here!
+			for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+			{
+				buffer[sample] = wavetable[(int)phase] * amplitude;
+				phase = fmod(phase + increment, wavetableSize);
+			}
+		}
 
-        // For more details, see the help for AudioProcessor::getNextAudioBlock()
+		phaseAtBufferStart = phase; //This ensures that the phase at the end of this buffer is the same as at the beginning of the next buffer
+	}
 
-        // Right now we are not producing any data, in which case we need to clear the buffer
-        // (to prevent the output of random noise)
-        bufferToFill.clearActiveBufferRegion();
-    }
+	void releaseResources() override
+	{
+		// This will be called when the audio device stops, or when it is being
+		// restarted due to a setting change.
 
-    void releaseResources() override
-    {
-        // This will be called when the audio device stops, or when it is being
-        // restarted due to a setting change.
+		// For more details, see the help for AudioProcessor::releaseResources()
+	}
 
-        // For more details, see the help for AudioProcessor::releaseResources()
-    }
+	//==============================================================================
+	void paint(Graphics& g) override
+	{
+		// (Our component is opaque, so we must completely fill the background with a solid colour)
+		g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 
-    //==============================================================================
-    void paint (Graphics& g) override
-    {
-        // (Our component is opaque, so we must completely fill the background with a solid colour)
-        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+		// You can add your drawing code here!
+	}
 
-        // You can add your drawing code here!
-    }
-
-    void resized() override
-    {
-        // This is called when the MainContentComponent is resized.
-        // If you add any child components, this is where you should
-        // update their positions.
-    }
+	void resized() override
+	{
+		// This is called when the MainContentComponent is resized.
+		// If you add any child components, this is where you should
+		// update their positions.
+	}
 
 
 private:
-    //==============================================================================
+	Array<float> wavetable;
+	int wavetableSize;
+	float frequency;
+	float phase, phaseAtBufferStart;
+	float increment;
+	float amplitude;
 
-    // Your private member variables go here...
-
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainContentComponent)
 };
 
 
 // (This function is called by the app startup code to create our main component)
-Component* createMainContentComponent()     { return new MainContentComponent(); }
+Component* createMainContentComponent() { return new MainContentComponent(); }
